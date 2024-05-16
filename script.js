86739c1638f33f0dea58fdc73d9d80245f9bef5b
pmac8 <29241044+pmac8@users.noreply.github.com>
2024-05-16 12:53:46 -0400

    Add styling and added new divs and classes to html as well as finished JS calculations

--- script.js
+++ script.js
@@ -1,7 +1,60 @@
-let firstNumber
-let operator
-let secondNumber
+const num = document.querySelectorAll('button')
+const display = document.querySelector('.display')
+const clear = document.querySelector('.clear')
 
+num.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const value = e.target.dataset.value
+
+        if(value === '='){
+            if(display.innerText.includes('/')) {
+                const splitStuff = display.innerText.split('/')
+                operate('/', splitStuff[0], splitStuff[1])
+            }
+            if(display.innerText.includes('*')) {
+                const splitStuff = display.innerText.split('*')
+                operate('*', splitStuff[0], splitStuff[1])
+            }
+            if(display.innerText.includes('+')) {
+                const splitStuff = display.innerText.split('+')
+                operate('+', splitStuff[0], splitStuff[1])
+            }
+            if(display.innerText.includes('-')) {
+                const splitStuff = display.innerText.split('-')
+                operate('-', splitStuff[0], splitStuff[1])
+            }
+        }
+        display.innerText += value
+    })
+})
+
+clear.addEventListener('click', () => {
+    display.innerText = ''
+})
+
+const operate = (operator,numOne,numTwo) => {
+    const valueOne = Number(numOne)
+    const valueTwo = Number(numTwo)
+    if(operator === '/'){
+        display.textContent = valueOne / valueTwo
+    }
+    if (operator === '+'){
+        display.textContent = valueOne + valueTwo
+    }
+    if (operator === '*'){
+        display.textContent = valueOne * valueTwo
+    }
+    if (operator === '-'){
+        display.textContent = valueOne - valueTwo
+    }
+}
+
+
+
+
+
+
+/* //basic math
 const add = (a , b) => {
     return a + b
 }
@@ -17,3 +70,25 @@ const multiply = (a , b) => {
 const divide = (a , b) => {
     return a / b
 }
+//valuables stored
+let firstNumber = ''
+let operatorValue = ''
+let secondNumber = '' */
+
+
+//function takes an operator and 2 numbers and then calls one of the above functions on the numbers
+/* const operate = (operator , numOne, numTwo) => {
+    if (operator === '+') {
+        return add(numOne,numTwo)
+    } else if (operator === '-') {
+        return subtract(numOne,numTwo)
+    } else if (operator === '*') {
+        return multiply(numOne,numTwo)
+    } else if (operator === '/') {
+        return divide(numOne,numTwo)
+    }
+} */
+
+
+
+
